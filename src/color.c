66ca36b644a4e6f5c398d006775dce31b3d2129c
myyla <amaluth29@gmail.com>
2023-08-05 08:24:25 +0100

    final push

--- src/color.c
+++ src/color.c
@@ -6,7 +6,7 @@
 /*   By: amtouham <amtouham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 15:06:09 by amtouham          #+#    #+#             */
-/*   Updated: 2023/08/04 13:52:28 by amtouham         ###   ########.fr       */
+/*   Updated: 2023/08/05 08:22:10 by amtouham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int	set_pixel_color(t_data *data, int value, int color)
 	if (!value)
 		return (0);
 	b = (int)((1 + sin(data->fctl * value + data->pctl)) \
-		/ 2 * 40.9 * (255 / 40));
-	r = (int)((1 + sin(data->fctl * value)) / 2 * 40.7 * (255 / 40));
+		/ 2 * 255);
+	r = (int)((1 + sin(data->fctl * value)) / 2 * 255 );
 	g = (int)((1 + sin(data->fctl * value + (data->pctl * 4.0))) \
-		/ 2 * 40.0 * (255 / 40));
+		/ 2 * 255);
 	color = (r << 16) | (g << 8) | b;
 	return (color);
 }
